3bcce6542c67e7c2c5f2c4b1f192e62e99596fff
Marcos Paulo Souza Miranda <marcospsmiranda@gmail.com>
2020-03-24 20:48:22 -0300

    Agrupamento do backend e frontend em um único repositório!

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const MongooseConfig = require('./config/MongooseConfig');
-
-const app = express();
-
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
+const app = express();
+
 console.log('Iniciando configurações do servidor...');
 
+app.use(cors());
+
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+
 
 app.use(routes);
 
